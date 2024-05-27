07ebbb6008250507f864ec887af866d5759c9e8e
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 09:46:52 +0500

    Add files via upload

---
+++ task35.js
@@ -0,0 +1,12 @@
+"use strict";
+//  task 35 
+let pet = ["Cat", "Goat", "Green Parrot"];
+console.log("Animals names :\n");
+for (let i = 0; i < pet.length; i++) {
+    console.log(`${pet[i]}`);
+}
+console.log("Statements :\n");
+for (let i = 0; i < pet.length; i++) {
+    console.log(`A  ${pet[i].toLowerCase()}  would make a great pet.`);
+}
+console.log("\nAny of these animals would make a great pets.\n\n");
---
+++ task35.ts
@@ -0,0 +1,19 @@
+//  task 35 
+let pet:string[] = ["Cat","Goat","Green Parrot"]
+console.log("Animals names :\n")
+for(let i=0; i<pet.length; i++)
+    {console.log(`${pet[i]}`)
+}
+    console.log("Statements :\n")
+for(let i=0; i<pet.length; i++){
+    console.log(`A  ${pet[i].toLowerCase()}  would make a great pet.`)
+
+}
+    console.log("\nAny of these animals would make a great pets.\n\n")
+
+
+
+
+
+
+
